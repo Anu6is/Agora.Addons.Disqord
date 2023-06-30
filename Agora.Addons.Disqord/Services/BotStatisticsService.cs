d1d3de9662863e28149dc2e247f876bb446c4037
Anu6is <curtis.mayers@gmail.com>
2023-06-30 09:02:06 -0400

    Agora API

---
+++ Agora.Addons.Disqord/Services/BotStatisticsService.cs
@@ -0,0 +1,21 @@
+﻿using Agora.Shared;
+using Agora.Shared.Attributes;
+using Agora.Shared.Services;
+using Disqord.Bot;
+using Disqord.Gateway;
+using Microsoft.Extensions.Logging;
+
+namespace Agora.Addons.Disqord
+{
+    [AgoraService(AgoraServiceAttribute.ServiceLifetime.Transient)]
+    public class BotStatisticsService : AgoraService, IBotStatisticsService
+    {
+        private DiscordBotBase Bot { get; }
+
+        public BotStatisticsService(DiscordBotBase bot, ILogger<BotStatisticsService> logger) : base(logger) => Bot = bot;
+
+        public int GetTotalGuilds() => Bot.GetGuilds().Count;
+
+        public int GetTotalMembers() => Bot.GetGuilds().Values.Sum(guild => guild.MemberCount);
+    }
+}

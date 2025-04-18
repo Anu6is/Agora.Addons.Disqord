1419b1463636c06fd3832c3d37f422c3aa45e8e6
Anu6is <curtis.mayers@gmail.com>
2025-04-18 13:37:34 -0400

    update packages

--- Agora.Addons.Disqord/Commands/Menus/View/ServerSettings/ToggleFeatureView.cs
+++ Agora.Addons.Disqord/Commands/Menus/View/ServerSettings/ToggleFeatureView.cs
@@ -16,7 +16,7 @@ namespace Agora.Addons.Disqord.Menus.View
         private readonly GuildSettingsContext _context;
         private readonly IDiscordGuildSettings _settings;
 
-        public static string[] InvertedSettings { get; } = { SettingsFlags.Donations.ToString() };
+        public static string[] InvertedSettings { get; } = [SettingsFlags.Donations.ToString()];
 
         public ToggleFeatureView(SettingsFlags flag,
                                  string featureText,

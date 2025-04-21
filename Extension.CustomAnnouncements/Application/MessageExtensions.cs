cfe6ae1e9c43dd1b230556d5ced8c3f1352ab0de
Anu6is <curtis.mayers@gmail.com>
2025-04-21 14:50:09 -0400

    add @here placeholder

--- Extension.CustomAnnouncements/Application/MessageExtensions.cs
+++ Extension.CustomAnnouncements/Application/MessageExtensions.cs
@@ -6,6 +6,10 @@ public static class MessageExtensions
 {
     public static string ReplacePlaceholders(string message, Dictionary<string, string> placeholders)
     {
+        placeholders["{@@everyone}"] = "@everyone";
+        placeholders.Add("@everyone", "@everyone");
+        placeholders.Add("@here", "@here");
+
         foreach (var placeholder in placeholders)
         {
             message = message.Replace($"{{{placeholder.Key}}}", Markdown.Bold(placeholder.Value), StringComparison.OrdinalIgnoreCase);
